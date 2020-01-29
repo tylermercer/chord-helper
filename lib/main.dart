7664e5454e73ada133d75375403401d09700b5b4
Tyler Mercer <tyleralanmercer@gmail.com>
2020-01-28 17:28:31 -0700

    Add method channel to dart layer

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,35 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  static const channel = const EventChannel("net.tylermercer.chordhelper/music");
-  Stream music = channel.receiveBroadcastStream().map<Map<String, dynamic>>((e) => jsonDecode(e as String));
+  static const eventChannel = const EventChannel("net.tylermercer.chordhelper/music");
+  Stream music = eventChannel.receiveBroadcastStream().map<Map<String, dynamic>>((e) => jsonDecode(e as String));
+
+  static const methodChannel = const MethodChannel("net.tylermercer.chordhelper/control");
+  bool isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initIsPlaying();
+
+    music.listen((data) {
+      if (data['command'] == "") {
+
+      }
+    });
+  }
+
+  void initIsPlaying() async {
+    methodChannel.invokeMethod("getMediaPlayState").then((playState) {
+      setState(() {
+        isPlaying = playState == "PLAYING";
+      });
+
+      if (playState == "PLAYING") {
+        methodChannel.invokeMethod("pauseMusic");
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,22 +64,66 @@ class _MainPageState extends State<MainPage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            StreamBuilder<Map<String, dynamic>>(
-              stream: music,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Text(snapshot.data.values.join(", "));
-                } else {
-                  return Text("No data");
-                }
-              }
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Center(
+              child: Text(
+                "A7",
+                style: Theme.of(context).textTheme.display4,
+              ),
             ),
-          ],
-        ),
+          ),
+          Container(
+            color: Colors.grey[300],
+            padding: EdgeInsets.all(16),
+            alignment: Alignment(0,0),
+            child: Theme(
+              data: Theme.of(context).copyWith(
+                primaryTextTheme: TextTheme(
+
+                )
+              ),
+              child: StreamBuilder<Map<String, dynamic>>(
+                stream: music,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data.values.join(", "));
+                  } else {
+                    return Text("No data");
+                  }
+                }
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.grey[300],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                IconButton(
+                  iconSize: 40,
+                  icon: Icon(Icons.skip_previous),
+                onPressed: () => methodChannel.invokeMethod("goToPreviousSong"),
+                ),
+                if (isPlaying) IconButton(
+                  iconSize: 40,
+                  icon: Icon(Icons.pause_circle_filled),
+                  onPressed: () => methodChannel.invokeMethod("pauseMusic"),
+                ) else IconButton(
+                  iconSize: 40,
+                  icon: Icon(Icons.play_circle_filled),
+                  onPressed: () => methodChannel.invokeMethod("playMusic"),
+                ),
+                IconButton(
+                  iconSize: 40,
+                  icon: Icon(Icons.skip_next),
+                  onPressed: () => methodChannel.invokeMethod("goToNextSong"),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
