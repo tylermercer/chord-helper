a50eca49b5649f0162987e46b2bad0bd3ccf0b85
Tyler Mercer <tyleralanmercer@gmail.com>
2020-01-31 22:28:47 -0700

    Implement API to get Song BPM from Spotify!

--- lib/get_song_bpm.dart
+++ lib/get_song_bpm.dart
@@ -1,6 +1,59 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'API_KEY.dart';
 import 'track_info.dart';
 
+import 'package:http/http.dart' as http;
+
 Future<int> getBpmFromTrackInfo(TrackInfo trackInfo) async {
-  return Future.delayed(Duration(milliseconds: 500))
-      .then((onValue) => trackInfo.name.hashCode);
+  String id = await _getTrackId(trackInfo);
+  return _getBpmFromTrackId(id);
+}
+
+Future<String> _getTrackId(TrackInfo trackInfo) async {
+  var query = {
+    'q' : '${trackInfo.artist} ${trackInfo.name}',
+    'type' : 'track',
+    'limit' : '1'
+  };
+  var url = "https://api.spotify.com/v1/search?${_encodeMap(query)}";
+
+  var response = await http.get(
+      url,
+      headers: {
+        "Accept": "application/json",
+        "Authorization": "Bearer $API_KEY"
+      }
+  );
+
+  debugPrint(response.body);
+
+  Map<String, dynamic> decoded = jsonDecode(response.body);
+  
+  return decoded['tracks']['items'][0]['id'];
+}
+
+
+Future<int> _getBpmFromTrackId(String id) async {
+  var url = "https://api.spotify.com/v1/audio-features/$id";
+
+  var response = await http.get(
+      url,
+      headers: {
+        "Accept": "application/json",
+        "Authorization": "Bearer $API_KEY"
+      }
+  );
+
+  debugPrint(response.body);
+
+  var object = jsonDecode(response.body);
+
+  return (object['tempo'] as double).round();
+}
+
+String _encodeMap(Map data) {
+  return data.keys.map((key) => "$key=${data[key]}").join("&").replaceAll(" ", "+");
 }
